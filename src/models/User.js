2d06ef20a36c227c1a03429ce739ad5445495c7a
Juliana Marques <marquesdjuliana@gmail.com>
2023-08-25 18:35:10 -0300

    Creating the User model in src/models/User.js with the correct properties

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    display_name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false, 
+    // underscored: true, 
+  });
+
+  return User;
+};
